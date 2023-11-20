91b3e77024dd52872addce41bfe6a4a1868cae17
MahnoorKhawaja <mahnoorkhawajaa2004@gmail.com>
2023-11-20 23:36:21 +0500

    classes

--- flex.cpp
+++ flex.cpp
@@ -1,7 +1,17 @@
 #include<iostream>
 using namespace std;
 
+class Student{};
+class course{};
+class system{};
+class filehandler{};
+class validator{};
+
+int main()
 {
+    cout<<"FLEX COURSE MANAGEMENT SYSTEM"<<endl;
+    cout<<"------------------------------"<<endl;
+    
     
 
   return 0;  
